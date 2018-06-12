e03ca735b0d82c8fc83999128b1348bb34b3cba2
squidfunk <martin.donath@squidfunk.com>
2018-06-12 10:14:31 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+[![Travis][travis-image]][travis-link]
+[![Gitter][gitter-image]][gitter-link]
+[![GitHub][github-image]][github-link]
+
+  [travis-image]: https://travis-ci.org/squidfunk/terraform-aws-api-gateway-enable-cors.svg?branch=master
+  [travis-link]: https://travis-ci.org/squidfunk/terraform-aws-api-gateway-enable-cors
+  [codecov-image]: https://img.shields.io/codecov/c/github/squidfunk/terraform-aws-api-gateway-enable-cors/master.svg
+  [codecov-link]: https://codecov.io/gh/squidfunk/terraform-aws-api-gateway-enable-cors
+  [gitter-image]: https://badges.gitter.im/squidfunk/terraform-aws-api-gateway-enable-cors.svg
+  [gitter-link]: https://gitter.im/squidfunk/terraform-aws-api-gateway-enable-cors
+  [github-image]: https://img.shields.io/github/release/squidfunk/terraform-aws-api-gateway-enable-cors.svg
+  [github-link]: https://github.com/squidfunk/terraform-aws-api-gateway-enable-cors/releases
+
+# Terraform AWS API Gateway Enable CORS
+
+TBD
+
+## License
+
+**MIT License**
+
+Copyright (c) 2018 Martin Donath
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to
+deal in the Software without restriction, including without limitation the
+rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+IN THE SOFTWARE.
---
+++ variables.tf
@@ -0,0 +1,81 @@
+# Copyright (c) 2018 Martin Donath <martin.donath@squidfunk.com>
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to
+# deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+
+# -----------------------------------------------------------------------------
+# Variables: API Gateway
+# -----------------------------------------------------------------------------
+
+# var.api_id
+variable "api_id" {
+  description = "API identifier"
+}
+
+# var.api_resource_id
+variable "api_resource_id" {
+  description = "API resource identifier"
+}
+
+# -----------------------------------------------------------------------------
+# Variables: CORS-related
+# -----------------------------------------------------------------------------
+
+# var.allow_headers
+variable "allow_headers" {
+  description = "Allowed headers"
+  type        = "list"
+
+  default = [
+    "Content-Type",
+    "X-Amz-Date",
+    "Authorization",
+    "X-Api-Key",
+    "X-Amz-Security-Token",
+  ]
+}
+
+# var.allow_methods
+variable "allow_methods" {
+  description = "Allowed methods"
+  type        = "list"
+
+  default = [
+    "OPTIONS",
+    "HEAD",
+    "GET",
+    "POST",
+    "PUT",
+    "PATCH",
+    "DELETE",
+  ]
+}
+
+# var.allow_origin
+variable "allow_origin" {
+  description = "Allowed origin"
+  type        = "string"
+  default     = "*"
+}
+
+# var.allow_max_age
+variable "allow_max_age" {
+  description = "Allowed response caching time"
+  type        = "string"
+  default     = "7200"
+}
