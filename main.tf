96fc1e942adee386195ad2cf4eeccdc675e1fa1d
Dwayne Bailey <dwayne@translate.org.za>
2019-12-02 15:48:04 +0000

    Cleanup 0.12: Interpolation-only expressions are deprecated

--- main.tf
+++ main.tf
@@ -24,17 +24,17 @@
 
 # aws_api_gateway_method._
 resource "aws_api_gateway_method" "_" {
-  rest_api_id   = "${var.api_id}"
-  resource_id   = "${var.api_resource_id}"
+  rest_api_id   = var.api_id
+  resource_id   = var.api_resource_id
   http_method   = "OPTIONS"
   authorization = "NONE"
 }
 
 # aws_api_gateway_integration._
 resource "aws_api_gateway_integration" "_" {
-  rest_api_id = "${var.api_id}"
-  resource_id = "${var.api_resource_id}"
-  http_method = "${aws_api_gateway_method._.http_method}"
+  rest_api_id = var.api_id
+  resource_id = var.api_resource_id
+  http_method = aws_api_gateway_method._.http_method
 
   type = "MOCK"
 
@@ -45,12 +45,12 @@ resource "aws_api_gateway_integration" "_" {
 
 # aws_api_gateway_integration_response._
 resource "aws_api_gateway_integration_response" "_" {
-  rest_api_id = "${var.api_id}"
-  resource_id = "${var.api_resource_id}"
-  http_method = "${aws_api_gateway_method._.http_method}"
+  rest_api_id = var.api_id
+  resource_id = var.api_resource_id
+  http_method = aws_api_gateway_method._.http_method
   status_code = 200
 
-  response_parameters = "${local.integration_response_parameters}"
+  response_parameters = local.integration_response_parameters
 
   depends_on = [
     aws_api_gateway_integration._,
@@ -60,12 +60,12 @@ resource "aws_api_gateway_integration_response" "_" {
 
 # aws_api_gateway_method_response._
 resource "aws_api_gateway_method_response" "_" {
-  rest_api_id = "${var.api_id}"
-  resource_id = "${var.api_resource_id}"
-  http_method = "${aws_api_gateway_method._.http_method}"
+  rest_api_id = var.api_id
+  resource_id = var.api_resource_id
+  http_method = aws_api_gateway_method._.http_method
   status_code = 200
 
-  response_parameters = "${local.method_response_parameters}"
+  response_parameters = local.method_response_parameters
 
   response_models = {
     "application/json" = "Empty"
