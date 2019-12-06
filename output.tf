27dad687afcc711dc32e51c553448457ebb65c4a
Dwayne Bailey <dwayne@translate.org.za>
2019-12-06 15:35:48 +0000

    Output all resources for outside access

---
+++ output.tf
@@ -0,0 +1,15 @@
+output "aws_api_gateway_method" {
+  value = aws_api_gateway_method._
+}
+
+output "aws_api_gateway_integration" {
+  value = aws_api_gateway_integration._
+}
+
+output "aws_api_gateway_integration_response" {
+  value = aws_api_gateway_integration_response._
+}
+
+output "aws_api_gateway_method_response" {
+  value = aws_api_gateway_method_response._
+}
