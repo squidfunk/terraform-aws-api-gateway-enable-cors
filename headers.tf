39563da6a4e71447962cc0977ba461b114ded5ef
Jiří Svěcený <sveceny@sitole.cz>
2021-04-18 10:14:49 +0200

    Changed deprecated map function to modern tomap

--- headers.tf
+++ headers.tf
@@ -24,13 +24,13 @@
 
 # local.*
 locals {
-  headers = map(
+  headers = tomap({
     "Access-Control-Allow-Headers", "'${join(",", var.allow_headers)}'",
     "Access-Control-Allow-Methods", "'${join(",", var.allow_methods)}'",
     "Access-Control-Allow-Origin", "'${var.allow_origin}'",
     "Access-Control-Max-Age", "'${var.allow_max_age}'",
     "Access-Control-Allow-Credentials", var.allow_credentials ? "'true'" : ""
-  )
+  })
 
   # Pick non-empty header values
   header_values = compact(values(local.headers))
